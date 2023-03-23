b9d8e7370c9fdbaa341112d273c841543ec87ac2
Ankur Choudhury <ankurchoudhury@Ankurs-MacBook-Pro.local>
2023-03-24 03:09:28 +0530

    removed description from required field

--- hooks/useForm.tsx
+++ hooks/useForm.tsx
@@ -7,10 +7,7 @@ const errMessages = {
 const allowedFileTypes = ["image/gif", "image/png", "image/jpeg"];
 const allowedFileSize = 60000;
 /*TODO:
- * - Refactor error handing ✅ 
  * - type interface for errors
- * - upload size validation ✅ 
- * - remove description from required field
  * - handle sdk errors - lit & bundlr
  */
 const useForm = <T,>(initialState: T, onSubmit: () => void) => {
@@ -20,31 +17,30 @@ const useForm = <T,>(initialState: T, onSubmit: () => void) => {
   const checkForErrors = (
     e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
   ) => {
-    if (e.target.type === "file") {
-      const file = (e as ChangeEvent<HTMLInputElement>).target.files?.[0];
+    const file = (e as ChangeEvent<HTMLInputElement>).target.files?.[0];
 
-      if (file) {
-        console.log(file.size)
-        let errorMessage = "";
+    if (e.target.type === "file" && file) {
+      console.log(file.size);
+      let errorMessage = "";
 
-        if (!allowedFileTypes.includes(file.type)){
-          errorMessage = `File Type is not allowed`
-        } else if (file.size > allowedFileSize) {
-          errorMessage = `File cannot be larger than ${allowedFileSize/1000}KB`
-        }
-
-        setErrors((prevState: any) => ({
-          ...prevState,
-          file: errorMessage
-        }));
+      if (!allowedFileTypes.includes(file.type)) {
+        errorMessage = `File Type is not allowed`;
+      } else if (file.size > allowedFileSize) {
+        errorMessage = `File cannot be larger than ${allowedFileSize / 1000}KB`;
       }
-    } else {
+
       setErrors((prevState: any) => ({
         ...prevState,
-        [e.target.name]: e.target.value
-          ? ``
-          : `${e.target.name} ${errMessages.required}`,
+        file: errorMessage,
       }));
+    } else {
+      e.target.hasAttribute("required") &&
+        setErrors((prevState: any) => ({
+          ...prevState,
+          [e.target.name]: e.target.value
+            ? ``
+            : `${e.target.name} ${errMessages.required}`,
+        }));
     }
   };
 
@@ -65,8 +61,10 @@ const useForm = <T,>(initialState: T, onSubmit: () => void) => {
     e.preventDefault();
     let formFilled = true;
     for (const property in formData) {
-      if (!formData[property]) {
+      const element = document.getElementsByName(property)[0];
+      if (element.hasAttribute("required") && !formData[property]) {
         formFilled = false;
+
         setErrors((prevState: any) => ({
           ...prevState,
           [property]: `${property} ${errMessages.required}`,
